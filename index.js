d59e7e0006d13930e42cf47b82385acd7b3bf6ec
Ryan Barker <ryangeorgebarker0@gmail.com>
2018-04-03 14:18:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function addToCart(item) {
 function viewCart() {
   var cartContents = []
   for (var i = 0; i < cart.length; i++) {
-    cartContents.push(itemAdd[itemName[i]] + 'at' + itemAdd[itemPrice[i]])
+    cartContents.push(itemName[i] + 'at' + itemPrice[i])
   }
   if (cart.length === 0) {
    return 'Your shopping cart is empty.'
