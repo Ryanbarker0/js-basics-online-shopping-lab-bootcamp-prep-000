0db9eb60dad837abc3757ab49829e85859c1da56
Ryan Barker <ryangeorgebarker0@gmail.com>
2018-04-03 14:02:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,9 @@ function addToCart(item) {
 }
 
 function viewCart() {
-  // write your code here
+  for (var i = 0; i < cart.length; i++) {
+    return ('In your cart, you have ' + `${cart[itemName[i]]} at ${cart[itemPrice[i]]}, ` + i++)
+  }
 }
 
 function total() {
